721288d7f4f45cde1ced132d15876784e632cd8c
yudai-1 <yudai.taguchi1024@gmail.com>
2020-10-27 18:11:45 +0900

    itemモデル、テーブル作成、ActiveStorageを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,12 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :category_id, :condition_id, :postage_id, :region_id, :shipping_date_id, :description, :image).merge(user_id: current_user.id)
+  end
   
 end
