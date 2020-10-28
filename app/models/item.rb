c3d41fff5e09803b73158c71ad1fc1af3ea3c53d
yudai-1 <yudai.taguchi1024@gmail.com>
2020-10-28 18:02:50 +0900

    itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :purchase
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :region_id
+    validates :shipping_date_id
+    validates :description
+    validates :image　
+  end
+
 end
