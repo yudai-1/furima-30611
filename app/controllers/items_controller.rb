5b242c283f49b9f8af49bd9cbceb43a79cfc22be
yudai-1 <yudai.taguchi1024@gmail.com>
2020-10-31 21:14:26 +0900

    商品情報編集機能修正-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @item = Item.all
   end
@@ -18,19 +19,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
 
   def edit
-    @item = Item.find(params[:id]) 
     unless current_user == @item.user
       redirect_to root_path
     end  
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -38,6 +36,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   
   private
