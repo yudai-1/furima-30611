09299dd1e775b7d5188bb75071f1cd87a9d194f0
yudai-1 <yudai.taguchi1024@gmail.com>
2020-10-31 20:03:26 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,25 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+
+  def edit
+    @item = Item.find(params[:id]) 
+    unless current_user == @item.user
+      redirect_to root_path
+    end  
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+
+  
   private
 
   def item_params
