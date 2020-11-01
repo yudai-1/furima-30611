22f4b2e436e078d1e3cbc5df76e4206d9252c392
yudai-1 <yudai.taguchi1024@gmail.com>
2020-11-01 20:42:59 +0900

    商品削除機能-修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user == @item.user
+      @item.destroy
+      redirect_to root_path
+    end
   end
   
   def set_item
